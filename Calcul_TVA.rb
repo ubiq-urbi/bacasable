820c7d52cfb1b547eae533861fa347681b68b0e0
G. Matuszewski <g.matuszewski@ubiq-urbi.fr>
2016-10-29 15:42:50 +0200

    reprise des noms de variable

--- Calcul_TVA.rb
+++ Calcul_TVA.rb
@@ -1,13 +1,20 @@
+#récupération des éléments saisis au clavier
+# .to_f permet de tout transformer en "floating"
+# attention au typafe, sinon ça marche moins bien...
+
 puts "saisir le montant HT"
-montant_HT = gets.to_f
+_montant_HT = gets.to_f
 
 puts "saisie le taux de TVA"
-taux_TVA = gets.to_f
-
-montant_TVA = montant_HT * taux_TVA
-montant_TTC = montant_HT + montant_TVA
+_taux_TVA = gets.to_f
 
 
-puts "montant HT : " + montant_HT.to_s
-puts "montant TVA : " + montant_TVA.to_s
-puts "Montant TTC : " + montant_TTC.to_s
+#Calcul du montant de TVA et du prix TTC
+_montant_TVA = _montant_HT * _taux_TVA
+_montant_TTC = _montant_HT + _montant_TVA
+
+
+#restitution des calculs
+puts "montant HT : " + _montant_HT.to_s
+puts "montant TVA : " + _montant_TVA.to_s
+puts "Montant TTC : " + _montant_TTC.to_s
