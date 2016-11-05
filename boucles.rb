b340bbecaf74e18933eebce99403d9bf2b563418
G. Matuszewski <g.matuszewski@ubiq-urbi.fr>
2016-11-05 19:37:03 +0100

    --

--- boucles.rb
+++ boucles.rb
@@ -1,8 +1,9 @@
+#initialisation des variables
 _prestation = Array.new(3)
 _prestation_table = Array.new {_prestation}
 
-puts _prestation
 
+#saisie clavier des valeurs des variables
 puts "libellé de la prestation : "
 _prestation_libelle = gets.to_s
 
@@ -13,16 +14,18 @@ puts "montant HT à facturer : "
 _prestation_montant_HT = gets.to_f
 
 
+#remplissage des tables
 _prestation = [_prestation_libelle , _prestation_TVA , _prestation_montant_HT]
-
 _prestation_table.push( _prestation )
 
-puts "taille de _prestation_table : " + _prestation_table.count.to_s
 
-i=0
-_prestation_table.each do |prestation|
-	puts i
-	puts _prestation_table
-	i = i + 1
-end
+# puts "taille de _prestation_table : " + _prestation_table.count.to_s
+
+# 
+# i=0
+# _prestation_table.each do |prestation|
+#	puts i
+#	puts _prestation_table
+#	i = i + 1
+# end
 
